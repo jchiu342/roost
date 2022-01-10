c8ced36a7aa4f6db4c20ea64d922947d16479022
Jeremy Chiu <jchiu342@gmail.com>
2022-01-10 15:48:48 -0800

    change waiting time

--- cpp/src/include/player/NNEvaluator.h
+++ cpp/src/include/player/NNEvaluator.h
@@ -109,7 +109,7 @@ public:
         // TODO: confirm that we evaluate predicate before locking for the first
         // time. otherwise there is a bug here
         using namespace std::chrono_literals;
-        cv_.wait_until(ul, now + 600ms, [this] { return done_processing_; });
+        cv_.wait_until(ul, now + 75ms, [this] { return done_processing_; });
         if (!done_processing_) {
           std::cout << "program will hang; feeding input\n";
           Tensor input_tensor = torch::from_blob(input_, {threads_, 5, BOARD_SIZE, BOARD_SIZE});
